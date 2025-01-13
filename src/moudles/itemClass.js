3e954c1ca8d9c96defaad6573256269791d8c549
null-heap <142169162+null-heap@users.noreply.github.com>
2025-01-13 15:17:55 +0200

    feat: improved composition

--- src/moudles/itemClass.js
+++ src/moudles/itemClass.js
@@ -9,27 +9,27 @@ function getId(){
 }
 
 
-// class ForToday {
-//   constructor(forToday) {
-//     this._forToday = forToday ? new Date() : false;
-//   }
+class ForToday {
+  constructor(forToday) {
+    this._forToday = forToday ? new Date() : false;
+  }
 
-//   get forToday() {
-//     if (this._forToday) {
-//       return isToday(this._forToday);
-//     } else {
-//       return false;
-//     }
-//   }
+  get forToday() {
+    if (this._forToday) {
+      return isToday(this._forToday);
+    } else {
+      return false;
+    }
+  }
 
-//   set forToday(value) {
-//     if (value) {
-//       this._forToday = new Date();
-//     } else {
-//       this._forToday = false;
-//     }
-//   }
-// }
+  set forToday(value) {
+    if (value) {
+      this._forToday = new Date();
+    } else {
+      this._forToday = false;
+    }
+  }
+}
 
 function Item(title, description, dueDate, priority, notes, status, forToday, id) {
       this.title = title;
@@ -37,27 +37,8 @@ function Item(title, description, dueDate, priority, notes, status, forToday, id
       this.dueDate = dueDate;
       this.priority = priority;
       this.notes = notes;
-      this._forToday = forToday ? new Date() : false;
+      this.forToday = new ForToday(forToday);
       this.status = status;
       this.id = id ? id : getId();
-
-
-      Object.defineProperty(this, "forToday", {
-        get: function(){
-          console.log(this._forToday);
-          if(this._forToday){
-            return isToday(this._forToday);
-          }else{
-            return false;
-          }
-        },
-        set: function(value){
-          if(value){
-            this._forToday = new Date();
-          }else{
-            this._forToday = false;
-          }
-        },
-      })
     }
 
